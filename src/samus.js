5e6de3934b7eaa0d1a772a57c731d1a022b5c9a5
meriadec <meriadec.pillet@gmail.com>
2016-08-17 19:49:26 +0200

    refactor(core): remove comments

--- src/samus.js
+++ src/samus.js
@@ -25,27 +25,3 @@ loadConfig()
     new Samus(url, config, program)
 
   })
-/*
-const url = process.argv[2] || ''
-
-const configFileName = path.join(process.env.HOME, '.samusrc')
-const config = {
-  defaultServer: null
-}
-
-fs.readFile(configFileName, { encoding: 'utf8' }, (err, l) => {
-  if (!err) {
-    try {
-      const loaded = JSON.parse(l)
-      Object.assign(config, loaded)
-    } catch (e) {
-      console.log(e)
-    }
-  }
-  if (!url && (!config || !config.defaultServer || !config.defaultServer.url)) {
-    console.log('usage: samus <url>')
-    process.exit(0)
-  }
-  new Samus(url, config)
-})
-*/
