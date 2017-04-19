234c74167e88d91aeb62c8b147880c5b85371ba5
meriadec <meriadec.pillet@gmail.com>
2017-04-19 21:19:13 +0200

    better config load

--- src/helpers/loadConfig.js
+++ src/helpers/loadConfig.js
@@ -1,24 +1,31 @@
 const fs = require('fs')
+const promisify = require('es6-promisify')
 const path = require('path')
+
+const readFile = promisify(fs.readFile)
 
 const configFileName = process.env.SAMUS_MOCK
   ? path.join(__dirname, '../../mock/samusrc')
   : path.join(process.env.HOME, '.samusrc')
 
-module.exports = () => new Promise((resolve, reject) => {
+module.exports = async function () {
 
   const config = {
     servers: [],
   }
 
-  fs.readFile(configFileName, { encoding: 'utf8' }, (err, loadedString) => {
-    if (!err) {
-      try {
-        const fileContent = JSON.parse(loadedString)
-        Object.assign(config, fileContent)
-      } catch (e) { return reject(e) }
+  try {
+    const fileContent = await readFile(configFileName, { encoding: 'utf8' })
+    try {
+      const parsedContent = JSON.parse(fileContent)
+      Object.assign(config, parsedContent)
+    } catch (e) {
+      throw new Error(`Bad config file: ${e.message}`)
     }
-    resolve(config)
-  })
+  } catch (e) {
+    throw new Error('Config file not found')
+  }
 
-})
+  return config
+
+}
