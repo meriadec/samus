33dd53388e1e6fd153ecd361ffd5f3674c6e2f90
meriadec <meriadec.pillet@gmail.com>
2017-04-19 21:30:01 +0200

    hide ../

--- src/samus.js
+++ src/samus.js
@@ -79,20 +79,22 @@ class Samus {
   }
 
   enrichItems (rawItems, url) {
-    return rawItems.map(text => {
-      let full = `${url}/${text}`
-      const isFolder = text[text.length - 1] === '/'
-      if (isFolder) {
-        full = full.substr(0, full.length - 1)
-      }
-      return {
-        name: text,
-        url: full,
-        isFolder,
-        isViewed: isViewed(full),
-        isSelected: !!this.playlist.find(u => u === full),
-      }
-    })
+    return rawItems
+      .filter(text => text !== '../')
+      .map(text => {
+        let full = `${url}/${text}`
+        const isFolder = text[text.length - 1] === '/'
+        if (isFolder) {
+          full = full.substr(0, full.length - 1)
+        }
+        return {
+          name: text,
+          url: full,
+          isFolder,
+          isViewed: isViewed(full),
+          isSelected: !!this.playlist.find(u => u === full),
+        }
+      })
   }
 
   play (url) {
